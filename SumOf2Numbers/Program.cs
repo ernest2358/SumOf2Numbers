067fda9586d0e372e1249262b9ff442609b22067
ernest2358 <ej2358@yahoo.com>
2020-02-20 11:22:19 -0500

    first commit

---
+++ SumOf2Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumOf2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number here:");
+            int firstNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a 2nd number here:");
+            int secondNum = int.Parse(Console.ReadLine());
+
+            int totalNum = firstNum + secondNum;
+            Console.Clear();
+            Console.WriteLine($"The sum of your 2 numbers: {firstNum} + {secondNum} = {totalNum}");
+        }
+    }
+}
